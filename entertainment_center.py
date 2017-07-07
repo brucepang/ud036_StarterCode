1c8065caa71b83acfbab6a0ce1400ab094bc3206
brucepang <brucepang@users.noreply.github.com>
2017-07-06 23:43:11 -0700

    Add files via upload
    changed style to conform to the guide line

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,29 +1,37 @@
 import media
 import fresh_tomatoes
+
+
 # construct six movie objects
-titanic = media.Movie("Titanic",
-	"The World's Most Beloved and Acclaimed Film",
-	"https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
-	"https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
-harry_potter_azkaban= media.Movie("Harry Potter", 
-	"The Grown-up Wizard Meets His Godfather",
-	"https://vignette3.wikia.nocookie.net/harrypotter/images/9/99/Harry-Potter-and-the-Prisoner-of-Azkaban-movie-poster.jpg/revision/latest?cb=20141215162758",
-	"https://www.youtube.com/watch?v=lAxgztbYDbs")
-gone_with_the_wind=media.Movie("Gone with the Wind",
-	"Recalling Civil War and Plantation Days of South",
-	"https://upload.wikimedia.org/wikipedia/en/b/b3/Gone_With_The_Wind_1967_re-release.jpg",
-	"https://www.youtube.com/watch?v=8mM8iNarcRc")
-roman_holiday=media.Movie("Roman Holiday",
-	"The Love between a Reporter and a Royal Princess",
-	"https://upload.wikimedia.org/wikipedia/en/b/b7/Roman_holiday.jpg",
-	"https://www.youtube.com/watch?v=9GzCG6lpFUw")
-inception = media.Movie("Inception",
-	"Dream on",
-	"https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-	"https://www.youtube.com/watch?v=66TuSJo4dZM")
-god_father = media.Movie("The Godfather","The Story of A New York Crime Family",
-	"http://celebmix.com/wp-content/uploads/2017/03/celebrating-45-years-of-the-godfather-01.jpg",
-	"https://www.youtube.com/watch?v=sY1S34973zA")
+titanic = media.Movie(
+    "Titanic",
+    "The World's Most Beloved and Acclaimed Film",
+    "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
+    "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+harry_potter = media.Movie(
+    "Harry Potter",
+    "The Grown-up Wizard Meets His Godfather",
+    "http://static.tvtropes.org/pmwiki/pub/images/harry_potter_and_the_prisoner_of_azkaban_ver5_4865.jpg",  # noqa
+    "https://www.youtube.com/watch?v=lAxgztbYDbs")
+wind = media.Movie(
+    "Gone with the Wind",
+    "Recalling Civil War and Plantation Days of South",
+    "https://upload.wikimedia.org/wikipedia/en/b/b3/Gone_With_The_Wind_1967_re-release.jpg",  # noqa
+    "https://www.youtube.com/watch?v=8mM8iNarcRc")
+roman = media.Movie(
+    "Roman Holiday",
+    "The Love between a Reporter and a Royal Princess",
+    "https://upload.wikimedia.org/wikipedia/en/b/b7/Roman_holiday.jpg",
+    "https://www.youtube.com/watch?v=9GzCG6lpFUw")
+inception = media.Movie(
+    "Inception",
+    "Dream on",
+    "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=66TuSJo4dZM")
+god_father = media.Movie(
+    "The Godfather", "The Story of A New York Crime Family",
+    "http://celebmix.com/wp-content/uploads/2017/03/celebrating-45-years-of-the-godfather-01.jpg",  # noqa
+    "https://www.youtube.com/watch?v=sY1S34973zA")
 # the method of fresh tomatoes takes in a list of movies as its input
-movies=[titanic,harry_potter_azkaban,gone_with_the_wind,roman_holiday,inception,god_father]
+movies = [titanic, harry_potter, wind, roman, inception, god_father]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,10 +1,28 @@
 import webbrowser
+"""
+This module contains Movie class.
+"""
+
 
 class Movie():
-	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-		self.title=movie_title
-		self.storyline=movie_storyline
-		self.poster_image_url=poster_image
-		self.trailer_youtube_url=trailer_youtube
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    """
+    Movie class that contains construtor and show_trailer method
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """
+        constructor that takes in movie's title, storyline, poster,
+        and trailer from youtube.
+        """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """
+        method that call the webbrowser module to open
+        the youtube website of trailer
+        """
+        webbrowser.open(self.trailer_youtube_url)
